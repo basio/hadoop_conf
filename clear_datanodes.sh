09c7b3c8e9cbd726c5c7746ba6d908aef50dd4eb
khalefa <m.elbasiouny@gmail.com>
2016-04-28 19:10:17 +0300

    v2

--- clear_datanodes.sh
+++ clear_datanodes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 		sudo rm -fr /disk1/hadoop/mydata/hdfs/namenode
 		sudo rm -fr /disk1/hadoop/mydata/hdfs/datanode
-		sudorm -fr /disk1/hadoop/tmp/
+		sudo rm -fr /disk1/hadoop/tmp/
 		 mkdir /disk1/hadoop/mydata/hdfs/namenode -p
 		#sudo chown mkhalefa:mkhalefa /disk1/hadoop/mydata/hdfs/namenode -R
 		 mkdir /disk1/hadoop/mydata/hdfs/datanode/current -p
@@ -23,9 +23,9 @@
 for HOST in graphsys-n01 graphsys-n02 graphsys-n03 graphsys-n04 graphsys-n05 graphsys-n06 graphsys-n07 graphsys-n08 graphsys-n09; do
 	ssh  -t  mkhalefa@$HOST " 
 		sudo rm -fr /usr/local/hadoop/etc/hadoop/*
-		 rm -fr /disk1/hadoop/mydata/hdfs/datanode
+	 	sudo rm -fr /disk1/hadoop/mydata/hdfs/
 		 mkdir /disk1/hadoop/mydata/hdfs/datanode/current -p
-		#sudo chown mkhalefa:mkhalefa /disk1/hadoop/mydata/hdfs/datanode/current -R
+		sudo chown mkhalefa:mkhalefa /disk1/hadoop/mydata/ -R
 	        rm -fr /disk1/hadoop/tmp
 		mkdir /disk1/hadoop/tmp
 		sudo mkdir /disk2/hadoop/1/yarn/local -p
@@ -34,6 +34,7 @@ for HOST in graphsys-n01 graphsys-n02 graphsys-n03 graphsys-n04 graphsys-n05 gra
 		sudo mkdir /disk2/hadoop/2/yarn/log -p
 		sudo mkdir /disk2/hadoop/3/yarn/local -p
 		sudo mkdir /disk2/hadoop/3/yarn/log -p
+		sudo chown mkhalefa:mkhalefa /disk2/hadoop -R
 		sudo chown mkhalefa:mkhalefa /disk2/hadoop/1/yarn/local -R
 		sudo chown mkhalefa:mkhalefa /disk2/hadoop/1/yarn/log -R
 		sudo chown mkhalefa:mkhalefa /disk2/hadoop/2/yarn/local -R
