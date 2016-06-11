9e293951af9b7ff4a7aef0ce2c8638d196b2548f
Khalefa <m.elbasiouny@gmail.com>
2016-06-11 03:37:25 +0200

    Create clear_log.sh

---
+++ clear_log.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+		sudo rm -f /usr/local/hadoop/logs/*
+		sudo rm -f /usr/local/hadoop/logs/userlogs/* 
+
+		
+for HOST in graphsys-n01 graphsys-n02 graphsys-n03 graphsys-n04 graphsys-n05 graphsys-n06 graphsys-n07 graphsys-n08 graphsys-n09; do
+	ssh  -t  mkhalefa@$HOST " 
+		sudo rm -f /usr/local/hadoop/logs/*
+		sudo rm -f /usr/local/hadoop/logs/userlogs/* 
+ 	"
+done
+
+
