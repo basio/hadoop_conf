e51d18a7b9b4bcfc364f836e62c17db6419346ab
khalefa <m.elbasiouny@gmail.com>
2016-06-12 13:12:23 +0300

    add collect logs

--- restart_cluster.sh
+++ restart_cluster.sh
@@ -3,8 +3,10 @@
 
 
 
-for HOST in graphsys-n00 graphsys-n01 graphsys-n02 graphsys-n03 graphsys-n04 graphsys-n05 graphsys-n06 graphsys-n07 graphsys-n08 graphsys-n09; do
+for HOST in  graphsys-n01 graphsys-n02 graphsys-n03 graphsys-n04 graphsys-n05 graphsys-n06 graphsys-n07 graphsys-n08 graphsys-n09; do
 	echo $HOST
-	ssh -t  mkhalefa@$HOST" sudo shutdown -r now"
+	ssh -t  mkhalefa@$HOST "
+			 sudo shutdown -r now
+	"
 done 
 
